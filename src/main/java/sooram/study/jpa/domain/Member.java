fcef8f16b196276b8c9960334ced3779e2d0f43d
sooram <ramhosoo@gmail.com>
2021-03-04 23:50:31 +0900

    회원등록 api완료

--- src/main/java/sooram/study/jpa/domain/Member.java
+++ src/main/java/sooram/study/jpa/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
