7f1da13a2d2265610dff2fcd3c897f71c4ca2e35
luke-ram <ramhosoo@gmail.com>
2021-03-06 10:57:56 +0900

    DTO 반환 & fetch조인 사용으로 쿼리 최적화 까지 완료

---
+++ src/main/java/sooram/study/jpa/api/OrderSimpleApiController.java
@@ -0,0 +1,60 @@
+package sooram.study.jpa.api;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sooram.study.jpa.domain.Address;
+import sooram.study.jpa.domain.Order;
+import sooram.study.jpa.domain.OrderStatus;
+import sooram.study.jpa.repository.OrderRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> orderV1() {
+        List<Order> all = orderRepository.findAll();
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2() {
+        List<Order> orders = orderRepository.findAll();
+        return orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> orderV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        return orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+}
--- src/main/java/sooram/study/jpa/repository/OrderRepository.java
+++ src/main/java/sooram/study/jpa/repository/OrderRepository.java
@@ -33,9 +33,16 @@ public class OrderRepository {
 
     }
 
-    public List<Order> findAll(){
+    public List<Order> findAll() {
         return em.createQuery("select o from Order o join o.member m", Order.class)
                 .getResultList();
     }
 
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        ).getResultList();
+    }
 }
